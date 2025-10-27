260fccf8b5f434e3311152ee864ff3ab2233c90c
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-10-27 11:35:06 +0300

    added componenets

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
-import React from "react";
 
-function About() {
+import React from "react";
+import Links from "./Links";
+
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+      {bio ? <p>{bio}</p> : null}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
+
+
 import user from "../data/user";
 console.log(user);
 
@@ -11,8 +13,12 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About
+        bio={user.bio}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
